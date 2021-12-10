3eaee3b9965d3f3f56ac5a3c642de7ca15273a48
ryan4gitee <rliang.lai@outlook.com>
2021-12-10 11:28:19 +0800

    Client UI logic scripts.

---
+++ TankWars_Client/Assets/Scripts/module/Room/RoomListPanel.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RoomListPanel : BasePanel
+{
+    private Text idText;
+    private Text scoreText;
+    private Button createBtn;
+    private Button refreshBtn;
+    private Transform content;
+
+    private GameObject roomObj;
+
+    public override void OnInit()
+    {
+        skinPath = "RoomListPanel";
+        layer = PanelManager.Layer.Panel;
+    }
+
+    public override void OnShow(params object[] args)
+    {
+        idText = skin.transform.Find("InfoPanel/IdText").GetComponent<Text>();
+        scoreText = skin.transform.Find("InfoPanel/ScoreText").GetComponent<Text>();
+        createBtn = skin.transform.Find("CtrlPanel/CreateBtn").GetComponent<Button>();
+        refreshBtn = skin.transform.Find("CtrlPanel/RefreshBtn").GetComponent<Button>();
+        content = skin.transform.Find("ListPanel/Scroll View/Viewport/Content");
+        roomObj = skin.transform.Find("Room").gameObject;
+
+        createBtn.onClick.AddListener(OnCreateClick);
+        refreshBtn.onClick.AddListener(OnRefreshClick);
+
+        roomObj.SetActive(false);
+        idText.text = GameMain.id;
+
+        NetManager.AddMsgListener("MsgGetAchieve", OnMsgGetAchieve);
+        NetManager.AddMsgListener("MsgGetRoomList", OnMsgGetRoomList);
+        NetManager.AddMsgListener("MsgCreateRoom", OnMsgCreateRoom);
+        NetManager.AddMsgListener("MsgEnterRoom", OnMsgEnterRoom);
+
+        MsgGetAchieve msgGetAchieve = new MsgGetAchieve();
+        NetManager.Send(msgGetAchieve);
+
+        MsgGetRoomList msgGetRoomList = new MsgGetRoomList();
+        NetManager.Send(msgGetRoomList);
+    }
+
+    private void OnJoinClick(string name)
+    {
+        MsgEnterRoom msg = new MsgEnterRoom();
+        msg.id = int.Parse(name);
+
+        NetManager.Send(msg);
+    }
+
+    private void OnRefreshClick()
+    {
+        MsgGetRoomList msg = new MsgGetRoomList();
+        NetManager.Send(msg);
+    }
+
+    private void OnCreateClick()
+    {
+        MsgCreateRoom msg = new MsgCreateRoom();
+        NetManager.Send(msg);
+    }
+
+    private void OnMsgGetRoomList(MsgBase msgBase)
+    {
+        MsgGetRoomList msg = msgBase as MsgGetRoomList;
+
+        for (int i = content.childCount - 1; i >= 0; i--)
+        {
+            GameObject o = content.GetChild(i).gameObject;
+            Destroy(o);
+        }
+
+        if (msg.rooms == null)
+        {
+            return;
+        }
+
+        for (int i = 0; i < msg.rooms.Length; i++)
+        {
+            GenerateRoom(msg.rooms[i]);
+        }
+    }
+
+    private void GenerateRoom(RoomInfo roomInfo)
+    {
+        GameObject o = Instantiate(roomObj);
+        o.transform.SetParent(content);
+        o.SetActive(true);
+        o.transform.localScale = Vector3.one;
+
+        Transform trans = o.transform;
+        Text idText = trans.Find("IdText").GetComponent<Text>();
+        Text countText = trans.Find("CountText").GetComponent<Text>();
+        Text stateText = trans.Find("StateText").GetComponent<Text>();
+        Button btn = trans.Find("JoinButton").GetComponent<Button>();
+
+        idText.text = roomInfo.id.ToString();
+        countText.text = roomInfo.count.ToString();
+
+        if (roomInfo.status == 0)
+        {
+            stateText.text = "准备中";
+        }
+        else
+        {
+            stateText.text = "战斗中";
+        }
+
+        btn.name = idText.text;
+
+        btn.onClick.AddListener(() =>
+        {
+            OnJoinClick(btn.name);
+        });
+    }
+
+    private void OnMsgGetAchieve(MsgBase msgBase)
+    {
+        MsgGetAchieve msg = (MsgGetAchieve)msgBase;
+
+        scoreText.text = $"{msg.win} 胜 {msg.lost} 负";
+    }
+
+    private void OnMsgEnterRoom(MsgBase msgBase)
+    {
+        MsgEnterRoom msg = msgBase as MsgEnterRoom;
+
+        if (msg.result == 1)
+        {
+            PanelManager.Open<RoomPanel>();
+            Close();
+        }
+        else
+        {
+            PanelManager.Open<TipPanel>("进入房间失败");
+        }
+    }
+
+    private void OnMsgCreateRoom(MsgBase msgBase)
+    {
+        MsgCreateRoom msg = msgBase as MsgCreateRoom;
+
+        if (msg.result == 1) {
+            PanelManager.Open<TipPanel>("创建成功");
+            PanelManager.Open<RoomPanel>();
+            Close();
+        }
+        else
+        {
+            PanelManager.Open<TipPanel>("创建房间失败");
+        }
+    }
+
+    public override void OnClose()
+    {
+        NetManager.RemoveMsgListener("MsgGetAchieve", OnMsgGetAchieve);
+        NetManager.RemoveMsgListener("MsgGetRoomList", OnMsgGetRoomList);
+        NetManager.RemoveMsgListener("MsgCreateRoom", OnMsgCreateRoom);
+        NetManager.RemoveMsgListener("MsgEnterRoom", OnMsgEnterRoom);
+    }
+}
---
+++ TankWars_Client/Assets/Scripts/module/Room/RoomPanel.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RoomPanel : BasePanel
+{
+    private Button startButton;
+    private Button closeButton;
+    private Transform content;
+    private GameObject playerObj;
+
+    public override void OnInit()
+    {
+        skinPath = "RoomPanel";
+        layer = PanelManager.Layer.Panel;
+    }
+
+    public override void OnShow(params object[] args)
+    {
+        startButton = skin.transform.Find("CtrlPanel/StartBtn").GetComponent<Button>();
+        closeButton = skin.transform.Find("CtrlPanel/CloseBtn").GetComponent<Button>();
+        content = skin.transform.Find("ListPanel/Scroll View/Viewport/Content");
+        playerObj = skin.transform.Find("Player").gameObject;
+
+        playerObj.SetActive(false);
+
+        startButton.onClick.AddListener(OnStartClick);
+        closeButton.onClick.AddListener(OnCloseClick);
+
+        NetManager.AddMsgListener("MsgGetRoomInfo", OnMsgGetRoomInfo);
+        NetManager.AddMsgListener("MsgLeaveRoom", OnMsgLeaveRoom);
+        NetManager.AddMsgListener("MsgStartBattle", OnMsgStartBattle);
+
+        MsgGetRoomInfo msg = new MsgGetRoomInfo();
+        NetManager.Send(msg);
+    }
+
+    private void OnCloseClick()
+    {
+        MsgLeaveRoom msg = new MsgLeaveRoom();
+        NetManager.Send(msg);
+    }
+
+    private void OnStartClick()
+    {
+        MsgStartBattle msg = new MsgStartBattle();
+        NetManager.Send(msg);
+    }
+
+    private void OnMsgStartBattle(MsgBase msgBase)
+    {
+        MsgStartBattle msg = msgBase as MsgStartBattle;
+
+        if (msg.result == 1)
+        {
+            this.Close();
+        }
+        else
+        {
+            PanelManager.Open<TipPanel>("开始失败！两队至少各需要一名玩家，只有队长可以开战！");
+        }
+    }
+
+    private void OnMsgLeaveRoom(MsgBase msgBase)
+    {
+        MsgLeaveRoom msg = msgBase as MsgLeaveRoom;
+        if (msg.result == 1)
+        {
+            PanelManager.Open<TipPanel>("退出房间");
+            PanelManager.Open<RoomListPanel>();
+            Close();
+        }
+        else
+        {
+            PanelManager.Open<TipPanel>("退出房间失败");
+        }
+    }
+
+    private void OnMsgGetRoomInfo(MsgBase msgBase)
+    {
+        MsgGetRoomInfo msg = msgBase as MsgGetRoomInfo;
+
+        for(var i = content.childCount - 1; i >= 0; i--)
+        {
+            GameObject o = content.GetChild(i).gameObject;
+            Destroy(o);
+        }
+
+        if (msg.players == null)
+        {
+            return;
+        }
+
+        for(var i = 0; i < msg.players.Length; i++)
+        {
+            GeneratePlayerInfo(msg.players[i]);
+        }
+    }
+
+    private void GeneratePlayerInfo(PlayerInfo playerInfo)
+    {
+        GameObject o = Instantiate(playerObj);
+        o.transform.SetParent(content);
+        o.SetActive(true);
+        o.transform.localScale = Vector3.one;
+
+        Transform trans = o.transform;
+        Text idText = trans.Find("IdText").GetComponent<Text>();
+        Text campText = trans.Find("CampText").GetComponent<Text>();
+        Text scoreText = trans.Find("ScoreText").GetComponent<Text>();
+
+        idText.text = playerInfo.id;
+        
+        if (playerInfo.camp == 1)
+        {
+            campText.text = "<color=#FF0000>红</color>";
+        }
+        else
+        {
+            campText.text = "<color=#0000FF>蓝</color>";
+        }
+
+        if (playerInfo.isOwner == 1)
+        {
+            campText.text = campText.text + "!";
+        }
+
+        scoreText.text = $"{playerInfo.win} 胜 {playerInfo.lost} 负";
+    }
+
+    public override void OnClose()
+    {
+        NetManager.RemoveMsgListener("MsgGetRoomInfo", OnMsgGetRoomInfo);
+        NetManager.RemoveMsgListener("MsgLeaveRoom", OnMsgLeaveRoom);
+        NetManager.RemoveMsgListener("MsgStartBattle", OnMsgStartBattle);
+    }
+}
